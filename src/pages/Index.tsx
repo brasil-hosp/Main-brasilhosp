2021f941d1ebf973127b999c442230bc3e0eeca9
Brasilhosp <grupofariabarbosa@brasil-hosp.com>
2025-11-17 11:41:18 -0300

    add partners to index

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,6 +3,7 @@ import Hero from "@/components/Hero";
 import About from "@/components/About";
 import Services from "@/components/Services";
 import WhyUs from "@/components/WhyUs";
+import Partners from "@/components/Partners";
 import Location from "@/components/Location";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
@@ -15,6 +16,7 @@ const Index = () => {
       <About />
       <Services />
       <WhyUs />
+      <Partners />
       <Location />
       <Contact />
       <Footer />
