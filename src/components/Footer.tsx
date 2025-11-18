2d2efb135e6ff1a2124c60b862f16acc2b177fdc
Brasilhosp <grupofariabarbosa@brasil-hosp.com>
2025-11-18 17:35:51 -0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -12,6 +12,7 @@ const Footer = () => {
     ],
     recursos: [
       { label: "Blog", href: "#" },
+      { label: "Catálogo", href: "/catalogo" },
       { label: "Certificações", href: "#" },
     ],
     legal: [
