685cf8bda5f65eb751172c471b139b15f009e1bc
Brasilhosp <grupofariabarbosa@brasil-hosp.com>
2026-03-03 11:36:29 -0300

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { ArrowRight, Phone } from "lucide-react";
-import heroImage from "@/assets/hero-hospital.jpg";
+import heroImage from "@/assets/hero-hospital.png";
 
 const Hero = () => {
   const scrollToContact = () => {
